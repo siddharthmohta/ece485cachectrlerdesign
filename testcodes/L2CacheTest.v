71c217d5dba078ce9063419c418fae435c6548b0
baltehsar <baltehsar@679579d4-c465-11de-8b01-0f61d55110ad>
2009-11-24 06:54:31 +0000

    cacheline fill works

--- testcodes/L2CacheTest.v
+++ testcodes/L2CacheTest.v
@@ -20,8 +20,12 @@
 */
 
 module L2CacheTest(stb, we_L1, addr_L1, stall, we_MEM, addr_MEM, data_L1, data_MEM,);
-  
-  // Parameter decleration
+
+/******************************************************************************
+                              PARAMETER DECLARATION
+******************************************************************************/
+
+  // General parameters
   parameter ADDR_WIDTH = 32;
   parameter DATA_WIDTH_L1 = 32;
   parameter HIGH_Z_L1 = 32'bz; //High impedance value for birdirectional bus
@@ -32,30 +36,48 @@ module L2CacheTest(stb, we_L1, addr_L1, stall, we_MEM, addr_MEM, data_L1, data_M
   parameter DATA_BUS_READ  = 1;
   parameter DATA_BUS_WRITE = 0;
   
-  parameter BURST_LENGTH = 2;
+  parameter BURST_LENGTH = 8;
   
-  // Cache Parameter declerataion
+  // Cache specific parameters
   parameter CACHE_WORD_WIDTH = 32;
+  parameter CACHE_TAG_WIDTH = 0;
   parameter CACHE_WAY_SIZE = 2;
   parameter CACHE_INDEX_SIZE = 2;
-  parameter CACHE_LINE_SIZE = 4;
+  parameter CACHE_LINE_SIZE = BURST_LENGTH * DATA_WIDTH_L2/CACHE_WORD_WIDTH;
+  //parameter CACHE_LINE_SIZE = BURST_LENGTH * 2 * CACHE_WORD_WIDTH;
+  parameter CACHE_PLRU_WIDTH = 3;  
 
-  // I/O port declarations    
-  input stb, we_L1, addr_L1;
 
-  output stall, we_MEM, addr_MEM;
+/******************************************************************************
+                                PORTS DEFINED
+******************************************************************************/
 
+  // I/O ports that interface L1Cache module
+  input we_L1, addr_L1;
+  output stall;
   inout [DATA_WIDTH_L1-1:0] data_L1;
+
+  assign data_L1  = (data_dir_L1) ? 64'bz : write_data_L1;
+                                                       //conditional assignment 
+                                                       //to bidirectional port
+  // I/O ports that interface MainMemory module
+  input stb;
+  output we_MEM, addr_MEM;
   inout [DATA_WIDTH_L2-1:0] data_MEM;
-  
-  // Conditional assignment to bidirectional data ports 
-  assign data_L1  = (data_dir_L1) ? 64'bz : write_data_L1; 
+
   assign data_MEM = (data_dir_MEM)? 64'bz : write_data_MEM;
+
   
-  // Net and variable decliration
+/******************************************************************************
+                    NETS, VARIABLES, DEFINITIONS, AND EVENTS
+******************************************************************************/  
+  
+  // Net declaration
   wire stb;
   wire [ADDR_WIDTH-1:0] addr_L1;
-  
+
+
+  // Register declaration
   reg stall, we_MEM;
   reg [ADDR_WIDTH-1:0] addr_MEM;
 
@@ -64,15 +86,104 @@ module L2CacheTest(stb, we_L1, addr_L1, stall, we_MEM, addr_MEM, data_L1, data_M
   reg [DATA_WIDTH_L2-1:0] write_data_MEM;
   
   reg [DATA_WIDTH_L2-1:0] data;
-  
-  // Initialize cache
+
+
+  // Counter, loop control veriables
+  integer burst_counter,line_counter,way_counter,word_counter;      
+  integer way, index, line, word;
+
+/******************************************************************************
+                            CACHE AND PLRU DEFINED
+******************************************************************************/   
+
+  // Cache declaration as arrays of registers
   reg [CACHE_WORD_WIDTH-1:0] cache_data [CACHE_WAY_SIZE-1:0][CACHE_INDEX_SIZE-1:0][CACHE_LINE_SIZE-1:0];
+  reg [CACHE_TAG_WIDTH-1:0] cache_tag [CACHE_WAY_SIZE-1:0][CACHE_INDEX_SIZE-1:0];
+  reg cache_dirty [CACHE_WAY_SIZE-1:0][CACHE_INDEX_SIZE-1:0];
+  reg cache_valid [CACHE_WAY_SIZE-1:0][CACHE_INDEX_SIZE-1:0];
+  
+  // Cache
+  //reg [CACHE_LINE_SIZE-1:0] cache[CACHE_INDEX_SIZE-1:0][CACHE_WAY_SIZE-1:0];
+  
+  // PLRU
+  reg [CACHE_PLRU_WIDTH-1:0] cache_plru[CACHE_INDEX_SIZE-1:0];
 
-  integer burst_counter,line_counter,way_counter,word_counter;        //counter
-
+/******************************************************************************
+                              INITIALIZATION
+******************************************************************************/
   // Initialize variables
   //initial
   //stall = 0; - shouldn't be done
+  
+  
+  // Initialize Cache and PLRU to zero values
+  initial
+  begin
+    
+    for (way = 0; way < CACHE_WAY_SIZE; way = way + 1)
+    begin
+      for(index = 0; index < CACHE_INDEX_SIZE; index = index + 1)
+      begin
+        for(word = 0; word < CACHE_LINE_SIZE; word = word + 1)
+          cache_data [way][index][word] = word;
+      end
+    end
+
+
+    for (index = 0; index < CACHE_INDEX_SIZE; index = index + 1)
+    begin
+      cache_plru [index] = 0;
+    end
+
+  /*
+  initial
+  begin
+
+    for (way = 0; way < CACHE_WAY_SIZE; way = way +1)
+    begin
+    
+      for(index = 0; index < CACHE_INDEX_SIZE; index = index + 1)
+      begin
+      
+        cache[way][index] = 0;
+      
+      end
+      
+    end
+      
+    for( index = 0; index < CACHE_INDEX_SIZE; index = index + 1 )
+    begin
+
+      plru[index] = 0;
+
+    end
+  */
+
+//testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+// Test Code to display all contents
+
+    for (way = 0; way < CACHE_WAY_SIZE; way = way + 1)
+    begin
+      for(index = 0; index < CACHE_INDEX_SIZE; index = index + 1)
+      begin
+        for(word = 0; word < CACHE_LINE_SIZE; word = word + 1)
+        begin
+          $display ("Way: %d Index: %d, Word: %d Content: %h", way, index, word, cache_data [way][index][word]);
+        end
+      end
+    end
+//testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+
+
+
+
+
+
+  end
+
+/******************************************************************************
+                        READ/WRITE REQUEST PROCESSING
+******************************************************************************/     
   
   // Process cache request when new address observed or we_L1 (de)asserted
   always @(addr_L1 or we_L1)
@@ -103,29 +214,51 @@ module L2CacheTest(stb, we_L1, addr_L1, stall, we_MEM, addr_MEM, data_L1, data_M
       we_MEM = 1;
       addr_MEM = addr_L1;
       word_counter = 0;
-      
-      repeat (BURST_LENGTH)
-        begin
 
-          @ (posedge stb or negedge stb)
-          begin
+      //filling a cache line
+      repeat (BURST_LENGTH)
+      begin
+  
+        @ (posedge stb or negedge stb)
+        begin
+  
+          data = data_MEM;
+
+          cache_data [0][0][word_counter]  = data[31:0];
+          cache_data [0][0][word_counter+1] = data[63:32];
           
-            data = data_MEM;
-            cache_data [0][0][word_counter] = data[31:0];
-            cache_data [0][0][word_counter+1] = data[62:32];
-          
-          end
-          
-          word_counter = word_counter + 2;
         end
 
-      $display ("cache_data [0][0][0]: %h", cache_data [0][0][0]);
-      $display ("cache_data [0][0][1]: %h", cache_data [0][0][1]);
-      
-      $display ("L2 write data: %h", write_data_L1);
+        word_counter = word_counter + 2;
+        
+      end
+  
+//testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+// Test Code to display all contents
+
+    for (way = 0; way < CACHE_WAY_SIZE; way = way + 1)
+    begin
+      for(index = 0; index < CACHE_INDEX_SIZE; index = index + 1)
+      begin
+        for(word = 0; word < CACHE_LINE_SIZE; word = word + 1)
+        begin
+          $display ("Way: %d Index: %d, Word: %d Content: %h", way, index, word, cache_data [way][index][word]);
+        end
+      end
     end
-    
+//testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+  
+        
+        $display ("L2 write data: %h", write_data_L1);
+        
+      end
+
     #1 stall = 0;
-  end
+
+    end    
+/******************************************************************************
+                                    TASKS
+******************************************************************************/
+  
   
 endmodule
