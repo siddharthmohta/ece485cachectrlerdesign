3130747fb803569908bf448c28ecd801b89ed62e
baltehsar <baltehsar@679579d4-c465-11de-8b01-0f61d55110ad>
2009-11-25 08:27:33 +0000

    Cache working with random replacement.
    Todo:
       LRU
       PLRU

--- currentcodes/L2CacheTest.v
+++ currentcodes/L2CacheTest.v
@@ -43,7 +43,7 @@ module L2CacheTest(stb, we_L1, addrstb_L1, addr_L1, stall, we_MEM, addrstb_MEM, 
   
   // Cache specific parameters
   parameter CACHE_WORD_SIZE = 32;
-  parameter CACHE_WAY_SIZE = 1;
+  parameter CACHE_WAY_SIZE = 2;
   parameter CACHE_INDEX_SIZE = 4;
   parameter CACHE_LINE_SIZE = BURST_LENGTH * DATA_WIDTH_L2/CACHE_WORD_SIZE;
   //parameter CACHE_LINE_SIZE = BURST_LENGTH * 2 * CACHE_WORD_SIZE;
@@ -196,91 +196,115 @@ module L2CacheTest(stb, we_L1, addrstb_L1, addr_L1, stall, we_MEM, addrstb_MEM, 
     
     Look_For_Match (addr_index, addr_tag, way, found);
 
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Cache Miss
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
     if(!found)
     begin
      
       $display("L2 MISS");
       
-            //assuming a cache miss
+      Look_For_Invalid (addr_index, way, found);
+       
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Replacement (if empty slot not found)
+    & Write back drity line
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/      
       
-      way = {$random} % CACHE_WAY_SIZE;
-
-      data_dir_MEM = 1;      
-      we_MEM = 1;
-      addr_MEM = addr_L1;
-      word_counter = 0;
+      if(!found)
+        Replacement (addr_index, way);
+        
+      //Write back a line
+      if(cache_dirty[way][addr_index])
+        Write_Back (addr_index, way);
       
-      #1;
-      
-      addrstb_MEM = ~addrstb_MEM;
-      
-      repeat (BURST_LENGTH)
+      Fill_Cache_Line (addr_tag, addr_index, way);
+/*
+      if(!we_L1)           //when we_L1 asserted, process L1 write
       begin
-
-        @ (posedge stb or negedge stb)
-        begin
-        
-          data = data_MEM;
-          cache_data [way][addr_index][word_counter] = data[31:0];
-          cache_data [way][addr_index][word_counter+1] = data[62:32];
-          
-        end
-         
-        word_counter = word_counter + 2;
-        
+    
+        cache_data[way][addr_index][addr_word] = data_L1;
+        cache_dirty[way][addr_index] = TRUE;
+      
+        $display("L1 Write");
+        $display("Data from L1: %h", data_L1);
+      
       end
       
-      cache_tag[way][addr_index] = addr_tag;
-      cache_valid[way][addr_index] = 1;
-      cache_dirty[way][addr_index] = 0;
+      else if(we_L1)      //when we_L1 de-asserted, process L1 read(=output data)
+      begin
       
+        write_data_L1 = cache_data[way][addr_index][addr_word];
+
+        $display("L1 Read");
+        $display ("L2 write data: %h", write_data_L1);
+      
+      end
+*/      
     end
     
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Cache Hit
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/    
     else if (found)
       $display("L2 HIT");
-    
-    if(!we_L1)           //when we_L1 asserted, process L1 write
-    begin
-    
-      //data_dir_L1 = DATA_BUS_READ; //set L1 data ports to high impedence
-      
-      cache_data[way][addr_index][addr_word] = data_L1;
-      
-      $display("L1 Write");
-      //data = data_L1;
-      $display("Data from L1: %h", data_L1);
-      
-    end
-    
-    else if(we_L1)      //when we_L1 de-asserted, process L1 read(=output data)
-    begin
-    
-      write_data_L1 = cache_data[way][addr_index][addr_word];
 
-      //data_dir_L1 = DATA_BUS_WRITE; //let write_data_L1 reg to drive the bus
-      $display("L1 Read");
-
-      $display ("L2 write data: %h", write_data_L1);
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Process Write Request from L1 (L1 writes to L2)
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+    
+      if(!we_L1)           //when we_L1 asserted, process L1 write
+      begin
       
-    end
+        cache_data[way][addr_index][addr_word] = data_L1;
+        cache_dirty[way][addr_index] = TRUE;
+        
+        $display("L1 Write");
+        $display("Data from L1: %h", data_L1);
+        
+      end
+    
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Process Read Request from L1 (L2 writes to L1)
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/    
 
+     else if(we_L1)      //when we_L1 de-asserted, process L1 read(=output data)
+     begin
+
+       if(cache_dirty[way][addr_index])
+       begin
+
+          Write_Back (addr_index, way);       $display ("way: %d", way);                 
+          Fill_Cache_Line (addr_tag, addr_index, way);
+          
+       end
+      
+
+       write_data_L1 = cache_data[way][addr_index][addr_word-1];
+ 
+       $display("L1 Read");
+       $display ("L2 write data: %h", write_data_L1);
+       
+     end
+   
 //testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
 // Test Code to display all contents
 
     for (way = 0; way < CACHE_WAY_SIZE; way = way + 1)
     begin
+      $display ("  way: %d", way);                  
       for(index = 0; index < CACHE_INDEX_SIZE; index = index + 1)
       begin
+        $display ("    index: %d", index);
         for(word = 0; word < CACHE_LINE_SIZE; word = word + 1)
         begin
-          $display ("Way: %d Index: %d, Word: %d Content: %h", way, index, word, cache_data [way][index][word]);
+          $display ("      Word: %d Content: %h", word, cache_data [way][index][word]);
         end
       end
     end
 //testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-      
-
-    
+   
     #1 stall = 0;
     
   end
@@ -288,6 +312,11 @@ module L2CacheTest(stb, we_L1, addrstb_L1, addr_L1, stall, we_MEM, addrstb_MEM, 
 /******************************************************************************
                                     TASKS
 ******************************************************************************/
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Look for the matching cache line
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
   task automatic Look_For_Match ( input [CACHE_INDEX_WIDTH-1:0] _index,
                                   input [CACHE_TAG_WIDTH-1:0] _tag,
                                   output [2:0] _way,
@@ -306,6 +335,103 @@ module L2CacheTest(stb, we_L1, addrstb_L1, addr_L1, stall, we_MEM, addrstb_MEM, 
     end
   end
   
-  endtask 
+  endtask
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Check if empty slot present
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+  task automatic Look_For_Invalid ( input [CACHE_INDEX_WIDTH-1:0] _index,
+                                    output [2:0] _way,
+                                    output _found );
+  begin      
+  
+      found = FALSE;
+          
+      for (way_counter = 0; way_counter < CACHE_WAY_SIZE; way_counter = way_counter + 1)
+      begin
+        
+        if (cache_valid[way_counter][_index])
+        begin
+
+          found = TRUE;
+          _way = way_counter;
+
+        end
+        
+      end  
+  end
+  
+  endtask
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Task: Replacement Policy
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+  task automatic Replacement ( input [CACHE_INDEX_WIDTH-1:0] _index,
+                               output [2:0] _way);
+
+  begin      
+            _way = {$random} % CACHE_WAY_SIZE;
+  end
+  
+  endtask
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Task: Evict_Cache_Line
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+  task automatic Write_Back (input [CACHE_INDEX_WIDTH-1:0] _index,
+                             input [2:0] _way);
+  begin
+    
+            $display("Write Back!");
+  
+  end
+  
+  endtask          
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Task: Fill_Cache_Line
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/  
+
+  task automatic Fill_Cache_Line (input [CACHE_TAG_WIDTH-1:0] _tag,
+                                  input [CACHE_INDEX_WIDTH-1:0] _index,
+                                  input [2:0] _way);
+  begin
+
+    data_dir_MEM = 1;      
+    we_MEM = 1;
+    addr_MEM = addr_L1;
+    word_counter = 0;
+      
+      #1;
+      
+      addrstb_MEM = ~addrstb_MEM;
+      
+      repeat (BURST_LENGTH)
+      begin
+
+        @ (posedge stb or negedge stb)
+        begin
+        
+          data = data_MEM;
+          cache_data [_way][_index][word_counter] = data[31:0];
+          cache_data [_way][_index][word_counter+1] = data[62:32];
+          
+        end
+         
+        word_counter = word_counter + 2;
+        
+      end
+      
+      cache_tag[_way][_index] = _tag;
+      cache_valid[_way][_index] = 1;
+      cache_dirty[_way][_index] = 0;  
+      
+      #2;
+      
+  end
+  endtask
     
 endmodule
